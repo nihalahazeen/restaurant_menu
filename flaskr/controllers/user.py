08b83dd97822c467553ac8b10bca25a39d09bfd2
Nihala <nihala.hazeen@edstem.com>
2022-08-30 12:07:38 +0400

    user update

--- flaskr/controllers/user.py
+++ flaskr/controllers/user.py
@@ -1,11 +1,9 @@
 from flask import Blueprint, jsonify
 from flaskr.daos.user import user_dao
-from flaskr.controllers.auth import is_admin
 
 user_api = Blueprint("user_api", __name__)
 
 @user_api.route("/get_users", methods=["GET"])
-@is_admin()
 def get_users():
     users = user_dao.list_all()
     user_list = [
